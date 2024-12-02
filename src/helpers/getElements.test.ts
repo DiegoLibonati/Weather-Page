38a3b7a5d095c10822d0d3ac27356bb9c3c57567
Die <dlibonati@leafnoise.io>
2024-12-01 23:57:14 -0300

    feat: better code tests

--- src/helpers/getElements.test.ts
+++ src/helpers/getElements.test.ts
@@ -1,27 +1,15 @@
 import { getElements } from "./getElements";
 
-const INITIAL_HTML: string = `
-    <main>
-      <section class="section_container">
-        <div class="section_container_search">
-          <h2>Wheater APP</h2>
-          <input type="text" placeholder="Search country" />
-          <button type="button" aria-label="search">SEARCH</button>
-        </div>
+import fs from "fs";
+import path from "path";
 
-        <img src="https://openweathermap.org/img/wn/04n@2x.png" alt="default img weather"/>
-
-        <div class="section_container_temp">
-          <h1></h1>
-          <h3></h3>
-        </div>
-      </section>
-    </main>
-`;
+const INITIAL_HTML: string = fs.readFileSync(
+  path.resolve(__dirname, "../../index.html"),
+  "utf8"
+);
 
 beforeEach(() => {
-  const body = INITIAL_HTML;
-
+  const body = INITIAL_HTML.match(/<body[^>]*>([\s\S]*?)<\/body>/i)![1];
   document.body.innerHTML = body;
 });
 
