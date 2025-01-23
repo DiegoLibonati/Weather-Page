136fcc8c813cdfa164e74c71619a9da6c0acc28e
Die <dlibonati@leafnoise.io>
2025-01-23 10:54:41 -0300

    feat: BEM CSS

--- src/helpers/getElements.ts
+++ src/helpers/getElements.ts
@@ -1,20 +1,20 @@
 export const getElements = () => ({
   searchButton: document.querySelector(
-    ".card__header button"
+    ".card__header-btn-search"
   ) as HTMLButtonElement,
   searchInput: document.querySelector(
-    ".card__header input"
+    ".card__header-input"
   ) as HTMLInputElement,
   weatherImg: document.querySelector(
-    ".card img"
+    ".card__img"
   ) as HTMLImageElement,
   countryName: document.querySelector(
-    ".card__header h2"
+    ".card__header-title"
   ) as HTMLHeadingElement,
   temperature: document.querySelector(
-    ".card__temp h1"
+    ".card__stats-temperature"
   ) as HTMLHeadingElement,
   temperatureDescription: document.querySelector(
-    ".card__temp h3"
+    ".card__stats-temperature-description"
   ) as HTMLHeadingElement,
 });
