a81ba00537d7af4834ab1a27537de2be844c1592
Die <dlibonati@leafnoise.io>
2026-04-18 16:32:59 -0300

    fix: requestSubmit prototype warning in tests

--- __tests__/jest.setup.ts
+++ __tests__/jest.setup.ts
@@ -3,6 +3,7 @@ import "@testing-library/jest-dom";
 import { mockEnvs } from "@tests/__mocks__/envs.mock";
 
 const mockFetch = jest.fn();
+const originalConsoleError = console.error.bind(console);
 
 global.fetch = mockFetch;
 
@@ -10,3 +11,13 @@ jest.mock("@/constants/envs", () => ({
   __esModule: true,
   default: mockEnvs,
 }));
+
+console.error = (...args: unknown[]): void => {
+  if (
+    String(args[0]).includes(
+      "Not implemented: HTMLFormElement.prototype.requestSubmit"
+    )
+  )
+    return;
+  originalConsoleError(...args);
+};
